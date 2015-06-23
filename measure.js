b53dee04fdfa43d1eee789f3ea7d64d6c89e06a0
Anders Hedberg <anders.hedberg@videocent.com>
2015-06-24 00:07:42 +0200

    Create measure.js

---
+++ measure.js
@@ -0,0 +1,15 @@
+var pmx = require('pmx');
+var probe = pmx.probe();
+
+var histogram = probe.histogram({
+  name        : 'latency',
+  measurement : 'mean'
+});
+
+var latency = 0;
+
+setInterval(function() {
+  latency = Math.round(Math.random() * 100);
+  histogram.update(latency);
+}, 100);
+
