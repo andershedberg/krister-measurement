4e41f2e979adb5f9464abcd83a960166d9c51500
Anders Hedberg <anders.hedberg@videocent.com>
2015-06-24 00:54:36 +0200

    Update measure.js

--- measure.js
+++ measure.js
@@ -1,3 +1,5 @@
+fs = require('fs')
+
 var pmx = require('pmx').init({
     network: true,
     ports: true
@@ -15,8 +17,22 @@ var metric = probe.metric({
 setInterval(function() {
   milliampere = Math.round(Math.random() * 100);
   var timestamp = Math.floor(new Date() / 1000);
-  pmx.emit('Internval Measurement', {
+  pmx.emit('Interval Measurement', {
                             "Timestamp": timestamp,
                             "mA" : milliampere
                         });
+  fs.readFile('/sys/bus/iio/devices/iio\:device0/in_voltage1_raw', 'utf8', function (err,data) {
+    milliampere = -1;
+    if (err) {
+      return console.log(err);
+    }
+    console.log(data);
+    milliampere = 0.06*data;
+    console.log(milliampere);
+  });
+/*  pmx.emit('Interval Measurement 2', {
+                            "Timestamp": timestamp,
+                            "mA" : data
+                        });
+*/
 }, 1000);
